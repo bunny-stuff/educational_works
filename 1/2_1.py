35e51258b1b21b7b7b6818173d59ff44702db6fd
suckduck <72158358+suckduck@users.noreply.github.com>
2021-11-26 19:23:46 +0300

    Исправлено задание 2_1

--- 1/2_1.py
+++ 1/2_1.py
@@ -1,13 +1,9 @@
-def calculate(x, w, b):
-    sum = 0
-    if len(x) != len(w): return -1
-    for n in x:
-        sum += x[n] * w[n] + b
-    return sum
+import numpy
+
 
 x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 0]
 w = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 b = 30
 
-solution = calculate(x, w, b)
+solution = numpy.dot(x, w) + b
 print(solution)
